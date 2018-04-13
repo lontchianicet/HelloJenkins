dda4c8ab0ea3366a94b3bbfefda5e025bfa6e1ae
ano <anicet.noutsua@ingecom.ch>
2018-04-14 00:25:37 +0200

    adding test file

---
+++ src/test/java/com/ingecom/tuto/MainTest.java
@@ -0,0 +1,15 @@
+package com.ingecom.tuto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	@DisplayName("Simple test Sum")
+	@Test
+	void test() {
+		assertEquals(1, 1);
+	}
+}
